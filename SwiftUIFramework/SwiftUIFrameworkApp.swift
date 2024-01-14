8b72544a65b4f40c7ccddf61cccda6278071b4f1
Kapil Goyal <kkgoyal@suyati.com>
2024-01-14 20:09:16 +0530

    Initial Commit

---
+++ SwiftUIFramework/SwiftUIFrameworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFrameworkApp.swift
+//  SwiftUIFramework
+//
+//  Created by Kapil Kumar on 14/01/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFrameworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
